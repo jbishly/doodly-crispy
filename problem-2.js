3ee3c1950f1447aef3a9bd6f9cfaf658267a6787
jbish <k.jennymh@gmail.com>
2017-03-11 04:47:28 +0900

    checked solution

--- problem-2.js
+++ problem-2.js
@@ -6,4 +6,23 @@ By starting with 1 and 2, the first 10 terms will be:
 By considering the terms in the Fibonacci sequence whose values do not exceed four million, 
 find the sum of the even-valued terms. */
 
+var fibonacci = [1,2];
+var num1 = 1;
+var num2 = 2;
+var fib = 0;
 
+while (fib < 4000000) {
+  fib = num1 + num2;
+  num1 = num2;
+  num2 = fib;
+  fibonacci.push(fib);
+}
+
+var answer = 0;
+
+for (i=0; fibonacci[i] < 4000000; i++) {
+  if (fibonacci[i] % 2 === 0) {
+      answer += fibonacci[i];
+  }
+}
+console.log(answer);
